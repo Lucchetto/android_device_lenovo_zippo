b2b7f89d70e9eb91f3f4947a79bf0b7506df10ca
Robert Shih <robertshih@google.com>
2019-11-25 22:25:41 -0800

    Enable v1.3 drm plugins
    Bug: 139134043
    Test: MediaDrmTest
    Change-Id: I7453b035ca29c7ee889d03de0d55e6d947b3cb77

--- device.mk
+++ device.mk
@@ -190,7 +190,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.drm@1.0-impl \
     android.hardware.drm@1.0-service-lazy \
-    android.hardware.drm@1.2-service.clearkey
+    android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint
 PRODUCT_COPY_FILES += \

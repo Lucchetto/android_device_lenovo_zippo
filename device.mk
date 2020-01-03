c3ed5739d76b42f91b7d1f77674749187c2c28a0
Demon Singur <demonsingur@gmail.com>
2020-01-04 03:20:30 +0530

    zippo: Build needed RIL packages
    Change-Id: Ie481b72ec2d4b4d932aa2715be97d3dc1eddfd46

--- device.mk
+++ device.mk
@@ -254,6 +254,11 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
+# RIL
+PRODUCT_PACKAGES += \
+    libril \
+    librilutils
+
 # Seccomp policy
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/seccomp_policy/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \

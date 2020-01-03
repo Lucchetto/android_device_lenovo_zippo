8d0c6403e48ade4f48466bd197737752906561c4
Demon Singur <demonsingur@gmail.com>
2020-01-04 03:21:16 +0530

    zippo: Build renderscript

--- device.mk
+++ device.mk
@@ -292,6 +292,10 @@ PRODUCT_PACKAGES += \
     rcs_service_api \
     rcs_service_api.xml
 
+# RenderScript
+PRODUCT_PACKAGES += \
+    android.hardware.renderscript@1.0-impl
+
 # RIL
 PRODUCT_PACKAGES += \
     libjson \

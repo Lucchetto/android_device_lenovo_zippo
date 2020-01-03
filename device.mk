aed7a02a10377d44c3012238191cf610f7a3e4c1
Demon Singur <demonsingur@gmail.com>
2020-01-04 03:06:29 +0530

    zippo: Copy needed camera permissions

--- device.mk
+++ device.mk
@@ -107,6 +107,12 @@ PRODUCT_PACKAGES += \
     libdng_sdk.vendor \
     Snap
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
+    frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
+    frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
+
 # Common init scripts
 PRODUCT_PACKAGES += \
     init.class_main.sh \

ca07830957525bfe13337fa9c971c385d9f97384
Demon Singur <demonsingur@gmail.com>
2020-01-28 03:41:41 +0530

    zippo: Build separate system and vendor HIDL libs

--- device.mk
+++ device.mk
@@ -212,8 +212,10 @@ PRODUCT_PACKAGES += \
 
 # HIDL
 PRODUCT_PACKAGES += \
-    android.hidl.base@1.0.vendor \
-    android.hidl.manager@1.0.vendor
+    android.hidl.base@1.0 \
+    android.hidl.base@1.0_system \
+    android.hidl.manager@1.0 \
+    android.hidl.manager@1.0_system
 
 # Input
 PRODUCT_COPY_FILES += \

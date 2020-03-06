7c0597986a74eff523f1781399e6cd7b7cdd4d15
Lucchetto <lucchetto.tie@live.com>
2020-03-06 11:05:19 +0100

    zippo: import perfboostsconfig.xml from ZUI 11.5.269
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -99,6 +99,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_SOONG_NAMESPACES += \
     $(LOCAL_PATH)
 
+# Perf
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/perf/perfboostsconfig.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/perf/perfboostsconfig.xml
+
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service

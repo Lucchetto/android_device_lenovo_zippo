8acb838ccbf14c50284659d9401efad2edfb57fb
Lucchetto <lucchetto.tie@live.com>
2020-06-11 10:26:05 +0200

    zippo: rename init.qcom.rc to init.custom.rc
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- rootdir/Android.mk
+++ rootdir/Android.mk
@@ -9,10 +9,10 @@ LOCAL_MODULE_PATH  := $(TARGET_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK
 include $(BUILD_PREBUILT)
 
 include $(CLEAR_VARS)
-LOCAL_MODULE       := init.qcom.rc
+LOCAL_MODULE       := init.custom.rc
 LOCAL_MODULE_TAGS  := optional
 LOCAL_MODULE_CLASS := ETC
-LOCAL_SRC_FILES    := etc/init.qcom.rc
+LOCAL_SRC_FILES    := etc/init.custom.rc
 LOCAL_MODULE_PATH  := $(TARGET_OUT_ETC)/init
 include $(BUILD_PREBUILT)
 

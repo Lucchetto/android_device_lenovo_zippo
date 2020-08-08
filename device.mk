0da65de5eac6e31dd8a29835141b404cbbb32470
Lucchetto <lucchetto.tie@live.com>
2020-08-08 20:29:45 +0200

    zippo: import camera blobs
        * From L78051_SECURE_USER_11.5.381_ST_200603
    
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I8f7461e3cef7d7e514e3dcd0c62a0df41dd86bb2

--- device.mk
+++ device.mk
@@ -127,8 +127,14 @@ PRODUCT_PACKAGES += \
 
 # Camera
 PRODUCT_PACKAGES += \
-    libdng_sdk.vendor \
-    Snap
+    android.frameworks.displayservice@1.0 \
+    android.hardware.camera.provider@2.4-impl \
+    android.hardware.camera.provider@2.4-service_64 \
+    libdng_sdk \
+    libgui_vendor \
+    libxml2 \
+    Snap \
+    vendor.qti.hardware.camera.device@1.0.vendor
 
 # Device init scripts
 PRODUCT_PACKAGES += \

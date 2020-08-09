5fcbec0687cc32f4413efa2270054a7bb479cf7c
Lucchetto <lucchetto.tie@live.com>
2020-08-09 11:40:59 +0200

    zippo: remove system copy of media_profiles_vendor.xml
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -289,9 +289,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_google_video_le.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media_profiles_vendor.xml:system/etc/media_profiles_vendor.xml
-
 # Network
 PRODUCT_PACKAGES += \
     netutils-wrapper-1.0

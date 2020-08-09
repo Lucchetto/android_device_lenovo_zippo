8dc5fe3b57e2fd7748ee16bb0fcec9c2e5ea34b3
Lucchetto <lucchetto.tie@live.com>
2020-08-09 08:01:27 +0200

    zippo: build rro overlay packages
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I7a3af525adbaf5d8710be840c55796ea2d936207

--- device.mk
+++ device.mk
@@ -27,6 +27,11 @@ DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
     $(LOCAL_PATH)/overlay-lineage
 
+PRODUCT_ENFORCE_RRO_TARGETS := *
+PRODUCT_ENFORCE_RRO_EXCLUDED_OVERLAYS += \
+    $(LOCAL_PATH)/overlay-lineage/lineage-sdk \
+    $(LOCAL_PATH)/overlay-lineage/packages/apps/Snap
+
 # Device uses high-density artwork where available
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi

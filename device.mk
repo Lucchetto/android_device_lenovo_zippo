2429eb567e3f3bbb595724b30548ba1fdde700b3
Lucchetto <lucchetto.tie@live.com>
2020-08-09 09:31:31 +0200

    zippo: rebrand pixel libperfmgr to lenovo
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -318,7 +318,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.3-service.pixel-libperfmgr
+    android.hardware.power@1.3-service.lenovo-libperfmgr
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/powerhint.json:$(TARGET_COPY_OUT_VENDOR)/etc/powerhint.json

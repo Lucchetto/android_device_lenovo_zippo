2f980bfe4dfd26ca62a79d8fcd3381a61e9efd70
Lucchetto <lucchetto.tie@live.com>
2020-08-11 21:50:05 +0200

    zippo: use prebuilt camera provider
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I3f3747b8d4c1eded52f23cfa76ed480a7525bec0

--- device.mk
+++ device.mk
@@ -130,13 +130,10 @@ PRODUCT_PACKAGES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service-lazy_64 \
     libdng_sdk \
     libgui_vendor \
     libxml2 \
-    Snap \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    Snap
 
 # Device init scripts
 PRODUCT_PACKAGES += \

7e48c0c957c79183d0ee2206218dff8d8b9ce8df
Lucchetto <lucchetto.tie@live.com>
2020-08-11 21:50:05 +0200

    zippo: use prebuilt camera provider
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -123,14 +123,11 @@ PRODUCT_COPY_FILES += \
 # Camera
 PRODUCT_PACKAGES += \
     android.frameworks.displayservice@1.0 \
-    android.hardware.camera.provider@2.4-impl \
-    android.hardware.camera.provider@2.4-service-lazy_64 \
     libdng_sdk \
     libgui_vendor \
     libqti-perfd-client \
     libxml2 \
-    Snap \
-    vendor.qti.hardware.camera.device@1.0.vendor
+    Snap
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \

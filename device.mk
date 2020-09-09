7070c13eeb1bc14b659a83591ce4a470a1082049
Lucchetto <lucchetto.tie@live.com>
2020-09-09 23:56:02 +0200

    zippo: build vendor libhwbinder
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -103,6 +103,10 @@ PRODUCT_COPY_FILES += \
     frameworks/av/services/audiopolicy/config/r_submix_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/r_submix_audio_policy_configuration.xml \
     frameworks/av/services/audiopolicy/config/usb_audio_policy_configuration.xml:$(TARGET_COPY_OUT_VENDOR)/etc/usb_audio_policy_configuration.xml \
 
+# Binder
+PRODUCT_PACKAGES += \
+    libhwbinder.vendor
+
 # Bluetooth
 PRODUCT_PACKAGES += \
     liba2dpoffload \

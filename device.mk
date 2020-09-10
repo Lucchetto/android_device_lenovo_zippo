1d1bbe07285082f237ecf9b201d881d17295ad14
Lucchetto <lucchetto.tie@live.com>
2020-09-10 08:43:38 +0200

    zippo: drop qti display mapper
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -184,9 +184,7 @@ PRODUCT_PACKAGES += \
     vendor.display.config@1.10 \
     vendor.qti.hardware.display.allocator-service \
     vendor.qti.hardware.display.allocator@1.0-service \
-    vendor.qti.hardware.display.allocator@1.0.vendor \
-    vendor.qti.hardware.display.mapper@2.0.vendor \
-    vendor.qti.hardware.display.mapperextensions@1.1.vendor
+    vendor.qti.hardware.display.allocator@1.0.vendor
 
 # DRM
 PRODUCT_PACKAGES += \

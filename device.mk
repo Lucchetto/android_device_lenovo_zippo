cbf1bff4e1777966203ea21a75890d5aa75b0831
Lucchetto <lucchetto.tie@live.com>
2020-09-30 10:57:18 +0200

    zippo: switch to OSS vibrator hal
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>
    Change-Id: I4ce0598a3b21884261782f745bdc63a4685b4930

--- device.mk
+++ device.mk
@@ -379,6 +379,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service
 
+# Vibrator
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.vibrator@1.2-service
+
 # WiFi
 PRODUCT_PACKAGES += \
     android.hardware.wifi@1.0-service-lazy \

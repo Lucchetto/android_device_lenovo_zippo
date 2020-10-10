7f289dbe3b5096b0397c0cc1febf8ec403d7dde5
Alessandro Astone <ales.astone@gmail.com>
2020-10-10 18:50:56 +0200

    zippo: Import codec2 seccomp policy
    Change-Id: I9efcb2f71a3917c4e317fdde754fc10905b91cc9

--- device.mk
+++ device.mk
@@ -336,6 +336,7 @@ PRODUCT_PACKAGES += \
 
 # Seccomp policy
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/seccomp_policy/codec2.vendor.base.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.base.policy \
     $(LOCAL_PATH)/seccomp_policy/codec2.vendor.ext.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/codec2.vendor.ext.policy \
     $(LOCAL_PATH)/seccomp_policy/mediacodec.policy:$(TARGET_COPY_OUT_VENDOR)/etc/seccomp_policy/mediacodec.policy
 

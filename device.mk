2b80db730d261623524cd3bc3b8bc1438b557d7b
Lucchetto <lucchetto.tie@live.com>
2020-10-30 14:20:17 +0100

    zippo: build aidl vibrator HAL
    Signed-off-by: Lucchetto <lucchetto.tie@live.com>

--- device.mk
+++ device.mk
@@ -421,7 +421,7 @@ PRODUCT_COPY_FILES += \
 
 # Vibrator
 PRODUCT_PACKAGES += \
-    vendor.qti.hardware.vibrator@1.2-service
+    vendor.qti.hardware.vibrator.service
 
 # Vulkan
 PRODUCT_PACKAGES += \

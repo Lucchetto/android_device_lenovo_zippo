14386eae4f7a5aebcb69669073caa4d1bebbc180
Adhitya Mohan <me@adhityamohan.in>
2020-11-15 15:48:46 +0530

    zippo: Update DRM HAL entries
    * Remove deprecated drm 1.0
    * push clearkey and widevine into seprate fragment files
    * import widevine manifest file from sunfish
    
    Change-Id: Ied04f06b89f3054cb95d4d5192007f9ead62d58a

--- device.mk
+++ device.mk
@@ -198,8 +198,6 @@ PRODUCT_PACKAGES += \
 
 # DRM
 PRODUCT_PACKAGES += \
-    android.hardware.drm@1.0-impl \
-    android.hardware.drm@1.0-service-lazy \
     android.hardware.drm@1.3-service.clearkey
 
 # Fingerprint

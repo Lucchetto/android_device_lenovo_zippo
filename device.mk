cbfd072724ad4761f0465262c2af0df0ebfee3a5
Marc Bourgoin <themard85@gmail.com>
2021-07-06 16:00:33 -0600

    zippo: Decommonize some audio configs
    Change-Id: I920a90c2d769cd65f8766ae0936ce37ee7a25ed4

--- device.mk
+++ device.mk
@@ -4,6 +4,11 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Audio
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
+    $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
+
 # Get non-open-source specific aspects
 $(call inherit-product-if-exists, vendor/lenovo/zippo/zippo-vendor.mk)
 

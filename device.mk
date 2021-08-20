a28e3b078d28302e4b1188ea40792126a3ba976d
Marc Bourgoin <themard85@gmail.com>
2021-08-19 23:58:34 -0600

    zippo: Decommonize power profile
    Change-Id: Ibb966f1517ef7c06d41a7e1c9e1179f6903ab8d8

--- device.mk
+++ device.mk
@@ -4,6 +4,10 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+# Overlays
+DEVICE_PACKAGE_OVERLAYS += \
+    $(LOCAL_PATH)/overlay
+
 # Audio
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \

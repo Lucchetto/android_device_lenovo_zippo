c858108a47c6ab19aa5516de552268e3932cba54
Marc Bourgoin <themard85@gmail.com>
2021-08-26 19:13:26 -0600

    zippo: Decommonize snap
    on heart, snap keeps defaulting the front camera to the IR camera where-as
    other camera apps correctly select the proper selfie camera
    
    Change-Id: I9e737c646f7c8b5d4596eb017ec87e8af0053935

--- device.mk
+++ device.mk
@@ -17,6 +17,10 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/mixer_paths_tavil.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_tavil.xml \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml
 
+# Camera
+PRODUCT_PACKAGES += \
+   Snap
+
 # FM
 PRODUCT_PACKAGES += \
     FM2 \
